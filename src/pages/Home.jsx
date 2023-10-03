5c3d3a182765d3befc7b6bdf843633d4186ac45f
fuaadtarmizi <fuaad2120@gmail.com>
2023-10-04 01:25:06 +0800

    in

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -25,7 +25,7 @@ const Home = () => {
         <div>
         <h1 className="text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl">Fuaad Tarmizi</h1>  
         <h2 className=" mt-3 lg:text-2xl font-medium tracking-tight text-slate-200 sm:text-xl">Software Developer</h2>
-        <p className="mt-4 max-w-xs text-lg leading-normal bg-opacity-5 text-slate-500">Im a software developer based in Damansara,Malaysia. Focus on Web Development and </p>
+        <p className="mt-4 max-w-xs text-lg leading-normal bg-opacity-5 text-slate-500">Im a software developer based in Damansara,Malaysia. </p>
         
         <nav className=" hidden lg:block">
           <ul className="mt-16 w-max">

4bafa6e5023ebc55c5fabeeda6369ce212786e7c
fuaadtarmizi <fuaad2120@gmail.com>
2023-10-04 14:44:37 +0800

    test

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,12 +11,7 @@ const Home = () => {
     <main className="scroll-smooth">  
     <body className="bg-slate-900 leading-relaxed text-slate-400 antialiased selection:bg-teal-300 selection:text-teal-900">
       <div id="__next">
-      
       <MouseEffect/>
-    
-      
-      
-        
       <div className="mx-auto max-w-screen-xl min-h-screen px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0">
       
       <a href="#content" className="absolute left-0 top-0 block -translate-x-full rounded bg-gradient-to-br from-teal-400 via-blue-500 to-purple-600 px-4 py-3 text-sm font-bold uppercase tracking-widest text-white focus-visible:translate-x-0">Skip to Content</a>

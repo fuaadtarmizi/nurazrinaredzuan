965f2f188546275d392af80b3c0bc81da744fbc6
fuaadtarmizi <fuaad2120@gmail.com>
2023-10-04 14:24:48 +0800

    we

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,7 +23,7 @@ const Home = () => {
       <div className="lg:flex lg:justify-between lg:gap-4">
       <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24">
         <div>
-        <h1 className="text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl">Fuaad Tarmizi</h1>  
+        <h1 className="text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl" href="https://fuaadtarmizii.vercel.app/" >Fuaad Tarmizi</h1>  
         <h2 className=" mt-3 lg:text-2xl font-medium tracking-tight text-slate-200 sm:text-xl">Software Developer</h2>
         <p className="mt-4 max-w-xs text-lg leading-normal bg-opacity-5 text-slate-500">Im a software developer based in Damansara,Malaysia. </p>
         
@@ -54,7 +54,7 @@ const Home = () => {
       <section className="ml-1 mt-8 flex items-center" aria-label="Social media">
       
         <div className="mr-5 text-xs">
-            <a className="block hover:text-slate-200" href="https://github.com/bchiang7" target="_blank" rel="noreferrer">
+            <a className="block hover:text-slate-200" href="https://github.com/fuaadtarmizi" target="_blank" rel="noreferrer">
               <span className="sr-only">GitHub</span>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="h-6 w-6" aria-hidden="true">
                 <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path>
@@ -70,7 +70,7 @@ const Home = () => {
             </a>
           </div>
           <div className="mr-5 text-xs">
-            <a className="block hover:text-slate-200" href="https://twitter.com/bchiang7" target="_blank" rel="noreferrer">
+            <a className="block hover:text-slate-200" href="https://twitter.com/" target="_blank" rel="noreferrer">
               <span className="sr-only">Twitter</span>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 248 204" fill="currentColor" className="h-6 w-6" aria-hidden="true">
                 <path id="white_background" d="M221.95,51.29c0.15,2.17,0.15,4.34,0.15,6.53c0,66.73-50.8,143.69-143.69,143.69v-0.04   C50.97,201.51,24.1,193.65,1,178.83c3.99,0.48,8,0.72,12.02,0.73c22.74,0.02,44.83-7.61,62.72-21.66   c-21.61-0.41-40.56-14.5-47.18-35.07c7.57,1.46,15.37,1.16,22.8-0.87C27.8,117.2,10.85,96.5,10.85,72.46c0-0.22,0-0.43,0-0.64   c7.02,3.91,14.88,6.08,22.92,6.32C11.58,63.31,4.74,33.79,18.14,10.71c25.64,31.55,63.47,50.73,104.08,52.76   c-4.07-17.54,1.49-35.92,14.61-48.25c20.34-19.12,52.33-18.14,71.45,2.19c11.31-2.23,22.15-6.38,32.07-12.26   c-3.77,11.69-11.66,21.62-22.2,27.93c10.01-1.18,19.79-3.86,29-7.95C240.37,35.29,231.83,44.14,221.95,51.29z"></path>
